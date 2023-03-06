5036498607564359d4ecf801f27e5611cfef59e5
johann8 <jo.hahn@gmx.de>
2023-03-06 11:21:50 +0100

    update to bacularis-app version 1.5.0; changed tags name

--- build_image.sh
+++ build_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # set variables
-_VERSION=0.1.4.0
+_VERSION=1.5.0
 
 # create build
-docker build -t johann8/bacularis:${_VERSION} .
+docker build -t johann8/bacularis:${_VERSION}-alpine .
 _BUILD=$?
 if ! [ ${_BUILD} = 0 ]; then
    echo "ERROR: Docker Image build was not successful"
@@ -12,14 +12,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a 
-   docker tag johann8/bacularis:${_VERSION} johann8/bacularis:latest
+   docker tag johann8/bacularis:${_VERSION}-alpine johann8/bacularis:latest-alpine
 fi
 
 #push image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/bacularis:latest
-   docker push johann8/bacularis:${_VERSION}
+   docker push johann8/bacularis:latest-alpine
+   docker push johann8/bacularis:${_VERSION}-alpine
    _PUSH=$?
    docker images -a |grep bacularis
 fi
@@ -28,9 +28,9 @@ fi
 #delete build
 if [ ${_PUSH=} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/bacularis:latest
+   docker rmi johann8/bacularis:latest-alpine
    #docker images -a
-   docker rmi johann8/bacularis:${_VERSION}
+   docker rmi johann8/bacularis:${_VERSION}-alpine
    #docker images -a
    #docker rmi ubuntu
    docker images -a
